9faa959488c5f985b341bbeb981e6a5aa26a6fd0
Kyle Bowen <kyle.m.bowen@gmail.com>
2023-12-15 17:43:17 -0700

    Set form handling to support base cases

--- app/controllers/weather_forecasts_controller.rb
+++ app/controllers/weather_forecasts_controller.rb
@@ -1,4 +1,23 @@
 class WeatherForecastsController < ApplicationController
+  before_action :set_session_location
+
   def index
+    if params[:location].blank? && session[:location].present?
+      redirect_to(weather_forecasts_url(location:)) and return
+    else
+      # get lat, long for location via geocoder
+      # get weather forecast via open-weather-ruby-client
+      render(:index, locals: { location: })
+    end
+  end
+
+  private
+
+  def location
+    session[:location]
+  end
+
+  def set_session_location
+    session[:location] =  params[:location].present? ? params[:location] : session[:location]
   end
 end
