572fb7e4d98c5b87c8d803003d8bc0b181361688
Kyle Bowen <kyle.m.bowen@gmail.com>
2023-12-18 14:29:38 -0700

    Add spec for WeatherServiceClient
    Special handling when testing required due to some limitations of the
    Singleton module in the Ruby stdlib.

---
+++ spec/lib/weather_service_client_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe WeatherServiceClient, type: :service do
+  let(:weather_service_client) {
+    # Workaround for the stdlib Singleton module that prevents calling `.new` on a class.
+    # We need to do this here so that the singleton instance doesn't leak between specs.
+    # Code should always access using `.instance`.
+    described_class.send(:new)
+  }
+  let(:open_weather) { instance_double('OpenWeather::Client', current_weather: { fake: :weather }) }
+  let(:api_key) { 'irrelephant' }
+
+  before do
+    allow(AppSettings).to receive(:open_weather_api_key).and_return(api_key)
+    allow(OpenWeather::Client).to receive(:new).with(api_key: api_key).and_return(open_weather)
+  end
+
+  describe '#current_weather' do
+    subject { weather_service_client.current_weather(lat: 40.7128, lon: -74.0060, lang: :en, units: :imperial) }
+
+    it 'calls current_weather on the OpenWeather client' do
+      expect(open_weather).to receive(:current_weather).with(
+        lat: 40.7128,
+        lon: -74.0060,
+        lang: :en,
+        units: :imperial
+      )
+      subject
+    end
+
+    it 'calls open_weather only once and caches the instance' do
+      expect(OpenWeather::Client).to receive(:new).once.and_return(open_weather)
+      2.times { weather_service_client.current_weather(lat: 40.7128, lon: -74.0060, lang: :en, units: :imperial) }
+    end
+  end
+
+  describe '#open_weather' do
+    subject { weather_service_client.open_weather }
+
+    it 'returns the OpenWeather client instance' do
+      expect(subject).to eq(open_weather)
+    end
+
+    it 'caches the OpenWeather client instance' do
+      expect(OpenWeather::Client).to receive(:new).once.and_return(open_weather)
+      2.times { weather_service_client.open_weather }
+    end
+  end
+end
+
