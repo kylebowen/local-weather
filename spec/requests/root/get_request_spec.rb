803c1b94b133e28fd99c4107f4ba61a22b6ceee2
Kyle Bowen <kyle.m.bowen@gmail.com>
2023-12-18 01:44:13 -0700

    Update specs to match expected behavior

--- spec/requests/root/get_request_spec.rb
+++ spec/requests/root/get_request_spec.rb
@@ -1,9 +1,15 @@
 require "rails_helper"
 
 RSpec.describe "/", type: :request do
-  it "loads the app without errors" do
-    get "/"
+  context "when the user has no previous sessions or searches" do
+    it "redirects to an appropriate page" do
+      get "/"
 
-    expect(response).to have_http_status(:ok)
+      expect(response).to redirect_to(new_weather_forecast_path)
+
+      follow_redirect!
+
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
--- spec/system/root_spec.rb
+++ spec/system/root_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe "root", type: :system do
   it "loads the app home page" do
     visit root_path
 
-    expect(page).to have_text("Search your Address, City, or Zip Code")
-    expect(page).to have_button("Search")
+    expect(page).to have_text("Local Weather")
+    expect(page).to have_field(placeholder: /Search for Address, City, or Zip Code/)
+    expect(page).to have_button(:search)
   end
 end
