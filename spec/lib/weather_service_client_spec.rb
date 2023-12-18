aead7409b6ee024f1bd9ebe601ab0f02934ebe05
Kyle Bowen <kyle.m.bowen@gmail.com>
2023-12-18 14:37:25 -0700

    Dry up method args

--- spec/lib/weather_service_client_spec.rb
+++ spec/lib/weather_service_client_spec.rb
@@ -9,6 +9,14 @@ RSpec.describe WeatherServiceClient, type: :service do
   }
   let(:open_weather) { instance_double('OpenWeather::Client', current_weather: { fake: :weather }) }
   let(:api_key) { 'irrelephant' }
+  let(:nyc_city_hall) {
+    {
+      lat: 40.7128,
+      lon: -74.0060,
+      lang: :en,
+      units: :imperial,
+    }
+  }
 
   before do
     allow(AppSettings).to receive(:open_weather_api_key).and_return(api_key)
@@ -16,21 +24,16 @@ RSpec.describe WeatherServiceClient, type: :service do
   end
 
   describe '#current_weather' do
-    subject { weather_service_client.current_weather(lat: 40.7128, lon: -74.0060, lang: :en, units: :imperial) }
+    subject { weather_service_client.current_weather(**nyc_city_hall) }
 
     it 'calls current_weather on the OpenWeather client' do
-      expect(open_weather).to receive(:current_weather).with(
-        lat: 40.7128,
-        lon: -74.0060,
-        lang: :en,
-        units: :imperial
-      )
+      expect(open_weather).to receive(:current_weather).with(**nyc_city_hall)
       subject
     end
 
     it 'calls open_weather only once and caches the instance' do
       expect(OpenWeather::Client).to receive(:new).once.and_return(open_weather)
-      2.times { weather_service_client.current_weather(lat: 40.7128, lon: -74.0060, lang: :en, units: :imperial) }
+      2.times { weather_service_client.current_weather(**nyc_city_hall) }
     end
   end
 
